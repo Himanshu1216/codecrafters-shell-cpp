5bdcce667cb527fa40461d5dc7b425ab41d11ffc
Himanshu <himanshu.student.eee21@itbhu.ac.in>
2025-12-19 13:49:06 +0530

    implement exit functionality

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@ int main() {
   while(true) {
     std::cout << "$ ";
     std::cin >> user_input;
+
+    if(user_input == "exit") {
+      return 0;
+    }
+
     std::cout << user_input << ": " << "command not found\n";
   }
 
