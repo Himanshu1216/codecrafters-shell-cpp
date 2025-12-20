9a2edf5a49a28e2a2cd009e2293e5a7370ad0315
Himanshu <himanshu.student.eee21@itbhu.ac.in>
2025-12-20 16:49:00 +0530

    implement type builtin command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,29 +9,48 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  std::string user_input;
+  std::vector<std::string> user_input;
+  std::string input;
   
   while(true) {
+    user_input.clear();
     std::cout << "$ ";
-    std::getline(std::cin, user_input);
+    std::getline(std::cin, input);
 
-    std::string comm1 = user_input, comm2 = "";
-    int len = user_input.size();
-    for(int i = 0; i < len; i++) {
-      if(user_input[i] == ' ') {
-        comm1 = user_input.substr(0, i);
-        comm2 = user_input.substr(i + 1, len - i - 1);
-        break;
+    std::string word = "";
+    for(int i = 0; i < input.size(); i++) {
+      if(input[i] == ' ') {
+        user_input.push_back(word);
+        word = "";
       }
+      else word += input[i];
     }
+    user_input.push_back(word);
 
-    if(comm1 == "exit") {
+    std::string cmd1 = user_input[0];
+
+    if(cmd1 == "type") {
+      for(int i = 1; i < user_input.size(); i++) {
+        if(user_input[i] == "exit" || user_input[i] == "echo") {
+          std::cout << user_input[i] << " is a shell builtin\n";
+        } 
+        else {
+          std::cout << user_input[i] << ": not found\n";
+        }
+      }      
+    }
+    else if(cmd1 == "exit") {
       return 0;
     }
-    else if(comm1 == "echo") {
-      std::cout << comm2 << '\n';
+    else if(cmd1 == "echo") {
+      for(int i = 1; i < user_input.size(); i++) {
+        std::cout << user_input[i] << ' ';
+      }
+      std::cout << '\n';
     }
-    else std::cout << user_input << ": " << "command not found\n";
+    else {
+      std::cout << "command not found\n";
+    }
   }
 
 }
